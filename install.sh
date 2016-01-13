6e093ff619e1cccc32aeb2778926169783f2f150
mika <mika@mika-mbp.local>
2016-01-13 23:37:28 +0100

    remove brew cask

--- install.sh
+++ install.sh
@@ -1,26 +1,59 @@
 #!/bin/bash
 
-# ohMyZSH symlinks
-sudo rm ~/.zshrc
-ln -s ~/.dotfiles/zsh/.zshrc ~/
-sudo rm -rf ~/.oh-my-zsh/custom
-ln -s ~/.dotfiles/zsh/custom ~/.oh-my-zsh/custom
-ln -s ~/.dotfiles/zsh/.aliases ~/.aliases
+install_homebrew () {
+  which -s brew
+  if [[ $? != 0 ]]; then
+    echo "• Installing Homebrew"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" </dev/null
 
+    brew install coreutils
+    brew install tree
+    brew install git
+    brew install imagemagick
+    brew install nvm
 
-# Install Tools / Apps 
-sh ./brew.sh
+    brew cleanup
+  fi
+}
 
+install_sublime () {
+  if [ -d "$HOME/Library/Application\ Support/Sublime\ Text\ 3" ]; then
+    echo "• Installing Sublime Text 3 config"
+    rm -rf ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+    ln -s ~/.dotfiles/sublime/User ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+    cp ~/.dotfiles/sublime/Package\ Control.sublime-package ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/
+  else
+    echo "• Sublime Text 3 not found"
+  fi
+}
 
-# Sublime Text 3
-sudo rm -rf ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
-ln -s ~/.dotfiles/sublime/User ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
-cp ~/.dotfiles/sublime/Package\ Control.sublime-package ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/
+install_node () {
+  export NVM_DIR=~/.nvm
+  source $(brew --prefix nvm)/nvm.sh
+  nvm install stable
+  nvm alias default stable
+}
 
-# nvm
-source $(brew --prefix nvm)/nvm.sh
-nvm install stable
-nvm alias default stable
+install_zsh () {
+  if [ -d "$HOME/.oh-my-zsh" ]; then
+    echo "• You already have Oh My Zsh installed."
+  else
+    echo "• Installing oh-my-zsh"
+    sudo chsh -s /bin/zsh $USER
+    git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
+    ln -s ~/.dotfiles/zsh/.zshrc ~/
+    rm -rf ~/.oh-my-zsh/custom
+    ln -s ~/.dotfiles/zsh/custom ~/.oh-my-zsh/custom
+    ln -s ~/.dotfiles/zsh/.aliases ~/.aliases
+  fi
+}
+
+install_homebrew
+install_sublime
+install_node
+install_zsh
+echo "please restart shell"
 
 
 # MySQL
